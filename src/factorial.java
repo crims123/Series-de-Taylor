276b11bec46dbc267d65afbcedb7ce7ba041f302
loktarLDLC <metalsteveen@gmail.com>
2016-08-10 11:50:19 -0500

    factorial y prueba

--- src/factorial.java
+++ src/factorial.java
@@ -2,6 +2,17 @@
 public class factorial
 {
 	
-	
+public static long factorial(int n){
+		
+		long fact = 1;
+		
+		for (int i = 1; i <= n; i++)
+		{
+			fact = fact*i;
+			
+		}
+		
+		return fact;
+	}
 
 }
--- src/prueba.java
+++ src/prueba.java
@@ -9,13 +9,37 @@ public class prueba
 		
 		int n;
 		int o;
+		long x = 0;
 		// TODO Auto-generated method stub
+		
 		System.out.println("bienvenido a calculo de series de taylor");
 		System.out.println("escriba un numero");
+		
 		n = in.nextInt();
 		System.out.println("ahora digite la opcion que desea realizar");
-		
+		System.out.println("si quiere factorial presione 1");
+		System.out.println("siq uiere seno presione 2");
+		System.out.println("si quiere potencia presione 3");
 		o = in.nextInt();
+		
+		if (o == 1)
+		{
+			x = factorial.factorial(n);
+			System.out.println("Factorial: " + x);
+
+		}
+		if (o == 2)
+		{
+			
+		}
+		if ( o == 3)
+		{
+			
+		}
+		
+		
+		
+		
 
 	}
 
